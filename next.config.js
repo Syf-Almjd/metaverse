7c859784c322a0e764aeb01dd16c030964ed68ce
Syf-Almjd <syfalmjd11@gmail.com>
2025-05-13 13:35:33 +0800

    changed constants

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/metaverse',
+  trailingSlash: true, // optional, improves static asset routing in some cases
+
   experimental: {
     appDir: false,
   },
