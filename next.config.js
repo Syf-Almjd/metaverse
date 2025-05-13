622971c97b0ad98053b2de57c1cf33687be1ef9e
Syf-Almjd <syfalmjd11@gmail.com>
2025-05-13 13:52:16 +0800

    new subdic config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/metaverse',
-  trailingSlash: true, // optional, improves static asset routing in some cases
   experimental: {
     appDir: false,
   },
